9e3b923974557a40d3bb271b4531255c828eaffe
Elias Jimenez Diz <eliasj.diz@gmail.com>
2023-04-05 15:00:33 -0500

    schemas

--- config/database.js
+++ config/database.js
@@ -3,5 +3,5 @@ import mongoose from "mongoose";
 mongoose.set('strictQuery', false)
 
 mongoose.connect(process.env.MONGO)
-    .then( () => console.log('Database Conectec!!!'))
+    .then( () => console.log('Database Connectec!!!'))
     .catch( error => console.log(error))
