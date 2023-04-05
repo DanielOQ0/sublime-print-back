7faacad1ccf7ee279c84cc0e02a93b8f29fc9e49
eliasdiz <114044365+eliasdiz@users.noreply.github.com>
2023-04-05 17:34:49 -0500

    Elias (#8)
    * creacion del middle validator para los esquemas del joi
    
    * schemas
    
    * schemas de user,products y category listos

--- models/Product.js
+++ models/Product.js
@@ -1,16 +1,16 @@
 import mongoose from 'mongoose'
 
 const schema = new mongoose.Schema(
-{
-name: { type: String, required: true },
-description: { type: String, required: true },
-image: { type: String, required: true },
-price: { type: Number, required: true },
-available_quantity: { type: Number, required: true }, // cantidad del producto disponible
-categories: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Category' }] //  permite la selección de varias categorías al crear o actualizar un producto
-},{
-timestamps: true
-}
+    {
+        name: { type: String, required: true },
+        description: { type: String, required: true },
+        image: { type: String, required: true },
+        price: { type: Number, required: true },
+        stock: { type: Number, required: true }, // cantidad del producto disponible
+        categories: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Category' }] //  permite la selección de varias categorías al crear o actualizar un producto
+        },{
+        timestamps: true
+    }
 )
 
 const Product = mongoose.model('Product',schema)
