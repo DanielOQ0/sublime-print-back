0e730ee8472dbdc462a355f7338fa2562e4f698f
DanielOQ <danielorozco0208@gmail.com>
2023-04-09 19:06:57 -0500

    fix

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"
 
 mongoose.set('strictQuery', false)
 
 mongoose.connect(process.env.MONGO_LINK)
     .then( () => console.log('Database Connectec!!!'))
-    .catch( error => console.log(error))
+    .catch( error => console.log(error,process.env.MONGO_LINK))
