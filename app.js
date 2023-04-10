eb9a978efca7c0d6534ae9897ad3022d35e069a4
Elias Jimenez Diz <eliasj.diz@gmail.com>
2023-04-10 08:27:07 -0500

    fix

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan'
 import indexRouter from './routes/index.js'
+import cors from 'cors'
 import { __dirname } from './utils.js'
 
 const app = express();
@@ -12,6 +13,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
