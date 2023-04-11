30ea0bb8ddc6d03e2c974a1d116d4a0fe179e23a
eliasdiz <114044365+eliasdiz@users.noreply.github.com>
2023-04-11 13:58:20 -0500

    Elias (#31)
    * fix
    
    * pequeños cambios pa que ande el signin y validaciones del joi
    
    * modelo cart, controller create
    
    * cart, get de todos los porductos de un usuario, populado por el product_id
    
    ---------
    
    Co-authored-by: FeerMeringer <58431140+FeerMeringer@users.noreply.github.com>

--- routes/carts.js
+++ routes/carts.js
@@ -2,11 +2,12 @@ import express from 'express'
 import controller from '../controllers/cart.js'
 import passport from '../middlewares/users/passport.js'
 
-const { create } = controller
+const { create, getAll } = controller
 
 const router = express.Router()
 
 router.post('/:id', passport.authenticate('jwt',{ session: false}), create)
+router.get('/', passport.authenticate('jwt',{ session: false}), getAll)
 
 
 export default router
