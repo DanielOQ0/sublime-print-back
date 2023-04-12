fee0f041d32160ab01a29093912bd3c61d84e67e
eliasdiz <114044365+eliasdiz@users.noreply.github.com>
2023-04-11 21:06:10 -0500

    Elias (#34)
    * fix
    
    * pequeños cambios pa que ande el signin y validaciones del joi
    
    * modelo cart, controller create
    
    * cart, get de todos los porductos de un usuario, populado por el product_id
    
    * CART, controladores de deleteOne y deleteAll
    
    ---------
    
    Co-authored-by: FeerMeringer <58431140+FeerMeringer@users.noreply.github.com>

--- routes/carts.js
+++ routes/carts.js
@@ -1,13 +1,18 @@
 import express from 'express'
 import controller from '../controllers/cart.js'
 import passport from '../middlewares/users/passport.js'
+import validator from '../middlewares/validator.js'
+import cart_update from '../schema/cartUpdate.js'
 
-const { create, getAll } = controller
+const { create, getAll, update, destroyOne, destroyAll } = controller
 
 const router = express.Router()
 
 router.post('/:id', passport.authenticate('jwt',{ session: false}), create)
 router.get('/', passport.authenticate('jwt',{ session: false}), getAll)
+router.put('/:id', passport.authenticate('jwt',{ session: false}), validator(cart_update), update )
+router.delete('/deleteOne/:id', passport.authenticate('jwt', { session: false}), destroyOne )
+router.delete('/', passport.authenticate('jwt', { session: false}), destroyAll )
 
 
 export default router
---
+++ schema/cartUpdate.js
@@ -0,0 +1,18 @@
+import Joi from 'joi'
+
+const schema = Joi.object({
+
+    cantidad: Joi
+    .number()
+    .required()
+    .min(0)
+    .positive()
+    .message({
+        'any.required': 'Quantity is required',
+        'number.empty': 'Quantity is required',
+        'number.min': 'Quantity must be more than 0',
+        'number.positive': 'Quantity must be a positive number',
+    }),
+})
+
+export default schema
