60c30e78a869098c80ce011e92703ccee9d19c73
Elias Jimenez Diz <eliasj.diz@gmail.com>
2023-04-16 12:40:01 -0500

    address completed

--- routes/addressUser.js
+++ routes/addressUser.js
@@ -3,14 +3,17 @@ import controller from '../controllers/addressUser.js'
 import passport from '../middlewares/users/passport.js'
 import validator from '../middlewares/validator.js'
 import schema from '../schema/address.js'
+import update_address from '../schema/addressUpdate.js'
 
-const { create, getAll, getOne } = controller
+const { create, getAll, getOne, update, destroyOne } = controller
 
 const router = express.Router()
 
 router.post('/', passport.authenticate('jwt', { session: false}), validator(schema), create)
 router.get('/', passport.authenticate('jwt', { session: false}), getAll)
 router.get('/:id', passport.authenticate('jwt', { session: false}), getOne)
+router.put('/:id', passport.authenticate('jwt', { session: false}), validator(update_address), update)
+router.delete('/:id', passport.authenticate('jwt', { session: false}), destroyOne)
 
 
 export default router
