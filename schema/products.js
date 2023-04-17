ece37385178caeb33c9f352f656a87c72538b014
eliasdiz <114044365+eliasdiz@users.noreply.github.com>
2023-04-17 09:09:19 -0500

    Elias (#56)
    * pequeños cambios en el controlador de userupdate
    
    * aja
    
    * fixed upload photo
    
    * direcciones con algunos controladores
    
    * address completed
    
    * fixed a products,para crear el producto con su respectiva imagen
    
    * fix products, no deja crear el producto si la imagen ya existe en el space
    
    * fix creacion de productos
    
    ---------
    
    Co-authored-by: FeerMeringer <58431140+FeerMeringer@users.noreply.github.com>

--- schema/products.js
+++ schema/products.js
@@ -1,8 +1,5 @@
 import Joi from "joi";
-// import BaseJoi from 'joi'
-// import { Extension } from "joi-filesize-extensions";
 
-// const Joi = BaseJoi.extend(Extension)
 
 const schema = Joi.object({
 
@@ -17,32 +14,6 @@ const schema = Joi.object({
             'string.max': 'Name must be 25 characters long max',
             'any.required': 'Name is required'
         }),
-    
-    key: Joi
-        .string()
-        .required()
-        .min(4)
-        .max(25)
-        .message({
-            'string.empty':'Key is required',
-            'string.min':'Key must be at least 4 characters',
-            'string.max': 'Key must be 25 characters long max',
-            'any.required': 'Key is required'
-        }),
-
-    description: Joi
-        .string()
-        .min(15)
-        .max(50)
-        .message({
-            'string.empty':'Description is required',
-            'string.min':'Description must be at least 15 characters',
-            'string.max': 'Description must be 50 characters long max',
-            'any.required': 'Description is required'
-        }),
-
-    // image: Joi
-    //     .validate('2 MG', Joi.number().filesize()).value,
     
     price: Joi
         .number()
@@ -66,6 +37,17 @@ const schema = Joi.object({
             'number.empty': 'Stock is required',
             'number.min': 'Stock must be more than 0',
             'number.positive': 'Stock must be a positive number',
+        }),
+
+    description: Joi
+        .string()
+        .min(15)
+        .max(50)
+        .message({
+            'string.empty':'Description is required',
+            'string.min':'Description must be at least 15 characters',
+            'string.max': 'Description must be 50 characters long max',
+            'any.required': 'Description is required'
         }),
 
 
