fdfae58c9516ac3ee6fda2febbc4c0ad7bb32dcd
DanielOQ <danielorozco0208@gmail.com>
2023-04-18 06:08:31 -0500

    correciones

--- controllers/payments/create.js
+++ controllers/payments/create.js
@@ -27,7 +27,7 @@ const controller = {
             to: req.user.email,
             subject: "Thank you for your purchase! Below we will show you the details of your order.",
             html: `<div  style=" text-align:center;" > 
-            <img  style="width: 100%; " src="http://localhost:3000/static/media/logo.69cd86d90139cc3bb574.png" alt="photo" /> 
+            <img  style="width: 100%; " src="https://sublimeprint.nyc3.digitaloceanspaces.com/logo.png" alt="photo" /> 
             <h3 style="font-size:20px; text-align:center">Thank you for your purchase!</h3>
             <p style="font-size:16px; text-align:center">Below we will show you the details of your order:</p>
             
